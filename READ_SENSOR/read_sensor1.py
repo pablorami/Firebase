6df9c49bea980e79c15deab4537b9ddea242d6a9
Daniel Rodriguez Atanes <51852489+danielrodriguezat@users.noreply.github.com>
2019-12-08 12:50:47 +0100

    Corregido codigo de alertas

--- READ_SENSOR/read_sensor1.py
+++ READ_SENSOR/read_sensor1.py
@@ -9,7 +9,7 @@ import requests
 #Tipo de sensor empleado
 sensor = Adafruit_DHT.AM2302 
 
-#Pin usado 
+#Pin de la Raspberry PI usado 
 pin = 4
 
 #Datos conexion de telegram
@@ -17,23 +17,28 @@ API_TOKEN = '1034926121:AAFoaCVrgLYNpCp-QC1dusW5bstKYlVJbxM'
 CHAT_ID = '13224841'
 URL_BASE = 'https://api.telegram.org/bot'
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+#Valores de alerta
+TEMPERATURA_MINIMA = float('20.00')
+HUMEDAD_MAXIMA = float('50.00')
+
+# Lectura de datos desde el sensor con el metodo read_retry que intenta leer hasta 15 veces (con 2 s entre lectura)
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
+#No siempre el valor podrá leerse, en ese caso habria que volver a leer
 if humidity is not None and temperature is not None:
     entrada ='*************VALORES ACTUALES *************'
     print(entrada)
     datos='Temperatura={0:0.1f}°C  Humedad={1:0.1f}%'.format(temperature, humidity)
     print(datos)
-    mensaje = 'Alerta! La temperatura de tu vivienda ha descendido de 20 ºC\n'
-    requests.post(URL_BASE+API_TOKEN+'/sendMessage?text='+mensaje+datos+' &chat_id='+CHAT_ID+'')
-    
+    if float(temperature) < TEMPERATURA_MINIMA:
+        mensaje = 'Alerta! La temperatura de tu vivienda ha descendido de 20 ºC\n'
+        print(mensaje)
+        requests.post(URL_BASE+API_TOKEN+'/sendMessage?text='+mensaje+datos+' &chat_id='+CHAT_ID+'')
+    if float(humidity) > HUMEDAD_MAXIMA:
+        mensaje = 'Alerta! La humedad de tu vivienda ha sobrepasado el 50%\n'
+        print(mensaje)
+        requests.post(URL_BASE+API_TOKEN+'/sendMessage?text='+mensaje+datos+' &chat_id='+CHAT_ID+'')
+        
 else:
     print('Error de lectura')
     sys.exit(1)
